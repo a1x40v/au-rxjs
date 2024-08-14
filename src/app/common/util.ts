050fa78e55049da9ce33099a4ddbcbae614d8038
a1x40v <a1x40v@gmail.com>
2024-08-14 11:05:03 +0300

    Create Observable. Operator map

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,17 @@
+import { Observable } from "rxjs";
 
-
+export function createHttpObservable(url: string) {
+  return new Observable((subscriber) => {
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((body) => {
+        subscriber.next(body);
+        subscriber.complete();
+      })
+      .catch((err) => {
+        subscriber.error(err);
+      });
+  });
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,33 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-
+import { Component, OnInit } from "@angular/core";
+import { Course } from "../model/course";
+import { interval, Observable, of, timer } from "rxjs";
+import {
+  catchError,
+  delayWhen,
+  map,
+  retryWhen,
+  shareReplay,
+  tap,
+} from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit() {
+    const http$ = createHttpObservable("/api/courses");
 
-    constructor() {
+    const courses$ = http$.pipe(map((res) => res["payload"]));
 
-    }
-
-    ngOnInit() {
-
-
-
-    }
-
+    courses$.subscribe({
+      next: (courses) => console.log(courses),
+      error: () => console.log("error"),
+      complete: () => console.log("complete"),
+    });
+  }
 }
