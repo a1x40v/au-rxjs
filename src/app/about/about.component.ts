e52ad6ef1b49957e99eb95d219a0e887855da67b
a1x40v <a1x40v@gmail.com>
2024-08-28 22:08:53 +0300

    Concat operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 
 import { createHttpObservable } from "../common/util";
 import { map } from "rxjs/operators";
+import { concat, interval, of } from "rxjs";
 
 @Component({
   selector: "about",
@@ -11,5 +12,11 @@ import { map } from "rxjs/operators";
 export class AboutComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const static$ = of(11, 22, 33);
+    const interval$ = interval(1000);
+    const concat$ = concat(static$, interval$);
+
+    concat$.subscribe(console.log);
+  }
 }
